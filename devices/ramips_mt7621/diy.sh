8025f99f2441a215bf7fb6465abd648ac87a6706
icons <65128346+gogogojason@users.noreply.github.com>
2022-06-12 14:07:50 +0800

    Update diy.sh

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -9,3 +9,5 @@ svn export --force https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/file
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += luci-app-natflow-users natflow-boot/' target/linux/ramips/Makefile
 
 rm -rf target/linux/generic/pending-5.10/444-mtd-nand-rawnand-add-support-for-Toshiba-TC58NVG0S3H.patch
+
+rm -f feeds/kiddin9/ntfs3-oot
